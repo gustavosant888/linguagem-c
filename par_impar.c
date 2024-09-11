735f5f6141a05525fb04e47a8dff6ca208cf191c
gustavo santos <gustavosant814@gmail.com>
2024-09-11 13:12:10 +0000

    arquivo mensagem de par ou impar para um valor

---
+++ par_impar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+ int num;
+ printf("digite um numero é:\n");
+ scanf("%d" ,&num);
+if(num % 2 == 0)
+{
+    printf("o numero digitado é par\n");
+}
+else{
+    
+printf("o numero digitado é impar");
+
+}
+return 0;
+}
